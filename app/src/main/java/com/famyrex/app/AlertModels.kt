5abea99ceb801920213622a3f3bfb84227ebcbc0
dorynexstudio-eng <dorynex.studio@gmail.com>
2026-09-05 00:54:04 +0200

    feat: extend alert types for protection and geofences

--- app/src/main/java/com/famyrex/app/AlertModels.kt
+++ app/src/main/java/com/famyrex/app/AlertModels.kt
@@ -2,7 +2,16 @@ package com.famyrex.app
 
 enum class AlertSeverity { INFO, ATTENTION, IMPORTANT }
 
-enum class AlertType { NIGHT_USE, APP_SPIKE, DAILY_LIMIT, PATTERN_CHANGE }
+enum class AlertType {
+    NIGHT_USE,
+    APP_SPIKE,
+    DAILY_LIMIT,
+    PATTERN_CHANGE,
+    PROTECTION_DEGRADED,
+    PROTECTION_RESTORED,
+    GEOFENCE_ENTER,
+    GEOFENCE_EXIT
+}
 
 data class SmartAlert(
     val id: String,
