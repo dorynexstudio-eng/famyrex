73635d269f98f2a42ecf4d042bb13b7a07c4c7b4
dorynexstudio-eng <dorynex.studio@gmail.com>
2026-09-05 20:58:29 +0200

    Open supervised device after family join

--- app/src/main/java/com/famyrex/app/SupervisedOnboardingActivity.kt
+++ app/src/main/java/com/famyrex/app/SupervisedOnboardingActivity.kt
@@ -1,11 +1,10 @@
 package com.famyrex.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 class SupervisedOnboardingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +13,10 @@ class SupervisedOnboardingActivity : ComponentActivity() {
             MaterialTheme {
                 JoinFamilyScreen(
                     context = applicationContext,
-                    onJoined = { finish() }
+                    onJoined = {
+                        startActivity(Intent(this, SupervisedDeviceActivity::class.java))
+                        finish()
+                    }
                 )
             }
         }
