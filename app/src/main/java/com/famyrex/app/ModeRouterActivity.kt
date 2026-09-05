56ea019714ddde59ae3fd292ccfe7927599359ff
dorynexstudio-eng <dorynex.studio@gmail.com>
2026-09-05 20:58:13 +0200

    Route supervised devices through onboarding

--- app/src/main/java/com/famyrex/app/ModeRouterActivity.kt
+++ app/src/main/java/com/famyrex/app/ModeRouterActivity.kt
@@ -7,8 +7,13 @@ import androidx.activity.ComponentActivity
 class ModeRouterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val target = if (FamilyStore(applicationContext).appMode() == FamyrexAppMode.SUPERVISED) {
-            Intent(this, SupervisedDeviceActivity::class.java)
+        val store = FamilyStore(applicationContext)
+        val target = if (store.appMode() == FamyrexAppMode.SUPERVISED) {
+            if (store.devices().any { it.linkState == DeviceLinkState.LINKED }) {
+                Intent(this, SupervisedDeviceActivity::class.java)
+            } else {
+                Intent(this, SupervisedOnboardingActivity::class.java)
+            }
         } else {
             Intent(this, MainActivity::class.java)
         }
