af2c51fc0b79a58a00d4bc6ebb57dc6e3303c12c
dorynexstudio-eng <dorynex.studio@gmail.com>
2026-09-06 16:48:14 +0200

    Fase 5: endurecer configuración segura de WebView

--- app/src/main/java/com/famyrex/app/WebSafetyController.kt
+++ app/src/main/java/com/famyrex/app/WebSafetyController.kt
@@ -2,7 +2,6 @@ package com.famyrex.app
 
 import android.webkit.WebSettings
 import android.webkit.WebView
-import androidx.webkit.WebViewCompat
 import androidx.webkit.WebViewFeature
 
 object WebSafetyController {
@@ -10,7 +9,12 @@ object WebSafetyController {
         webView.settings.apply {
             javaScriptEnabled = false
             domStorageEnabled = false
+            allowFileAccess = false
+            allowContentAccess = false
+            allowFileAccessFromFileURLs = false
+            allowUniversalAccessFromFileURLs = false
             setSupportMultipleWindows(false)
+            setSupportZoom(false)
             mixedContentMode = WebSettings.MIXED_CONTENT_NEVER_ALLOW
             if (WebViewFeature.isFeatureSupported(WebViewFeature.SAFE_BROWSING_ENABLE)) {
                 WebSettingsCompatBridge.enableSafeBrowsing(this, settings.blockKnownThreats)
