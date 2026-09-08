61f9c7a994627f61c61a618abe0126665edfafe0
dorynexstudio-eng <dorynex.studio@gmail.com>
2026-09-08 16:13:23 +0200

    test: add Robolectric runtime for Android unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.json:json:20240303")
     testImplementation("androidx.test:core:1.6.1")
+    testImplementation("org.robolectric:robolectric:4.14.1")
 }
