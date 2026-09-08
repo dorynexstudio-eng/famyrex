a12035cade21bd76ebda34dda2814b60b60e185a
dorynexstudio-eng <dorynex.studio@gmail.com>
2026-09-08 16:13:35 +0200

    test: run remote command executor tests with Robolectric

--- app/src/test/java/com/famyrex/app/FamilyRemoteCommandExecutorTest.kt
+++ app/src/test/java/com/famyrex/app/FamilyRemoteCommandExecutorTest.kt
@@ -6,7 +6,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class FamilyRemoteCommandExecutorTest {
     private val context: Context = ApplicationProvider.getApplicationContext()
 
