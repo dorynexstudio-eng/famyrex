7e335b88856f92e8a659f88b489119ce20e3b3fb
dorynexstudio-eng <dorynex.studio@gmail.com>
2026-09-08 15:15:57 +0200

    fix: use Firebase SDKs compatible with Kotlin 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.14.1"))
+    implementation(platform("com.google.firebase:firebase-bom:33.5.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-functions")
