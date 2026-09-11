320cda7feb2fae0e37727d8fb19f397f115d376d
dorynexstudio-eng <dorynex.studio@gmail.com>
2026-09-11 08:51:38 +0200

    feat: export secure family snapshot callable

--- firebase/functions/src/main.ts
+++ firebase/functions/src/main.ts
@@ -2,3 +2,4 @@ export * from "./index";
 export { deleteMyAccount } from "./deleteMyAccount";
 export { createSecureParentInvite, acceptSecureParentInvite } from "./secureParentInvite";
 export { createPendingChildProfile } from "./childProfile";
+export { getSecureFamilySnapshot } from "./familySnapshot";
